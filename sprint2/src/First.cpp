69ea54875bfad6b548dc710ff34f7d5da681e387
Team1batman <106199562+Team1batman@users.noreply.github.com>
2022-05-25 06:51:15 +0530

    Update First.cpp

--- sprint2/src/First.cpp
+++ sprint2/src/First.cpp
@@ -1,8 +1,28 @@
+/**********************************************************************************
+**
+** FILENAME : First.cpp
+**
+** DESCRIPTIION : 
+**
+**    CREATED BY                   DATE
+**---------------------------------------------------------
+** 	Team-1          	24-05-2022
+**
+********************************************************************************/
 #include<iostream>
 #include<string>
 #include"First.h"
 using namespace std;
 
+/*********************************************************************
+**
+** Function NAME: First() 
+**
+** DESCRIPTION  : Declaring name,password,age,mobile for accessing.
+**
+** RETURNS : No return
+**
+**********************************************************************/
 First::First()
 {
 	name="NULL";
@@ -11,6 +31,15 @@ First::First()
 	mobile="NULL";
 }
 
+/*********************************************************************
+**
+** Function NAME: setData(string name,string password,int age,string mobile) 
+**
+** DESCRIPTION  : Declaring Set function for name,password,age,mobile using string function.
+**
+** RETURNS : No return
+**
+**********************************************************************/
 void First :: setData(string name,string password,int age,string mobile)
 {
 	this->name=name;
@@ -19,17 +48,44 @@ void First :: setData(string name,string password,int age,string mobile)
         this->mobile=mobile;
 }
 
+/*********************************************************************
+**
+** Function NAME: getName() 
+**
+** DESCRIPTION  : This Function gets name from User.
+**
+** RETURNS : No return
+**
+**********************************************************************/
 string First :: getName()
 {
 	return name;
 }
 
+/*********************************************************************
+**
+** Function NAME: getMobile() 
+**
+** DESCRIPTION  : This Function gets Mobile number from User.
+**
+** RETURNS : No return
+**
+**********************************************************************/
 string First :: getMobile()
 {
 	return mobile;
 
 }
 
+/*********************************************************************
+**
+** Function NAME: getPassword() 
+**
+** DESCRIPTION  : This Function gets Password from User.
+**
+** RETURNS : No return
+**
+**********************************************************************/
 string First :: getPassword()
 {
 	return password;
