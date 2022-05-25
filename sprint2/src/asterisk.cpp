370e2401de7931a49c69a2c18c2b785eaf75bfa2
Team1batman <106199562+Team1batman@users.noreply.github.com>
2022-05-25 07:02:27 +0530

    Update asterisk.cpp

--- sprint2/src/asterisk.cpp
+++ sprint2/src/asterisk.cpp
@@ -1,3 +1,14 @@
+/**********************************************************************************
+**
+** FILENAME : asterisk.cpp
+**
+** DESCRIPTIION : It will convert and mask the Users input into asterisk(*).
+**
+**    CREATED BY                   DATE
+**---------------------------------------------------------
+** 	Team-1          	24-05-2022
+**
+********************************************************************************/
 #include <termios.h>
 #include <unistd.h>
 #include<stdio_ext.h>
@@ -6,26 +17,37 @@
 #include <string>
 using namespace std;
 
-int getch() {
+/*********************************************************************
+**
+** Function NAME: getch() 
+**
+** DESCRIPTION  : Converting the input characters using terminal commands from termios header .
+**
+** RETURNS : int
+**
+**********************************************************************/
+int getch()
+{
     int ch;
     struct termios t_old, t_new;
-
     tcgetattr(STDIN_FILENO, &t_old);
     t_new = t_old;
     t_new.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &t_new);
-
     ch = getchar();
-
     tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
 	__fpurge(stdin);
     return ch;
 }
-
-
-
-
-
+/*********************************************************************
+**
+** Function NAME: getch() 
+**
+** DESCRIPTION  : Storing password into string and changing those into asterisk (*).
+**
+** RETURNS : int
+**
+**********************************************************************/
 string getpass(const char *prompt, bool show_asterisk=true)
 {
   const char BACKSPACE=127;
